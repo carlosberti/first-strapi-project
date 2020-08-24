179877d0e0635b9b0c4a765b0d6c12d6ee1538e5
Carlos Berti <carlosberti182@gmail.com>
2020-08-24 16:46:58 -0300

    Landing Page cms 1.0

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cffe1ba7c637ca6e0c518a381027548f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd807825d-037b-4f0f-adb1-83e4b795632c'
+};
